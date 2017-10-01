b9e2676426f9d3839645316d0ee5fa907da3e622
Alexandru Florian Barascu <alex.florin235@yahoo.com>
2017-10-01 16:07:46 +0300

    Added navigation params between the teams and team details pages.

--- src/pages/team-detail/team-detail.ts
+++ src/pages/team-detail/team-detail.ts
@@ -1,19 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Globals } from "../../app/shared/globals";
+import { Team } from "../../app/shared/interfaces";
 
 @IonicPage({ name: Globals.PAGE_NAMES.teamDetails })
 @Component({
   selector: 'page-team-detail',
   templateUrl: 'team-detail.html',
 })
-export class TeamDetailPage {
+export class TeamDetailPage implements OnInit {
+  public team: Team;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  constructor(public navCtrl: NavController, public navParams: NavParams) {}
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad TeamDetailPage');
+  ngOnInit(): void {
+    this.team = this.navParams.data.team;
+    console.log(this.team)
   }
 
 }
--- src/pages/teams/teams.ts
+++ src/pages/teams/teams.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { Globals } from "../../app/shared/globals";
 import { TeamDetailPage } from "../team-detail/team-detail";
+import { Team } from "../../app/shared/interfaces";
 
 @IonicPage({ name: Globals.PAGE_NAMES.teams })
 @Component({
@@ -10,11 +11,17 @@ import { TeamDetailPage } from "../team-detail/team-detail";
 })
 export class TeamsPage {
 
+  teams: Team[] = [
+    { id: 1, name: 'HC Elite' },
+    { id: 2, name: 'Team Takeover' },
+    { id: 3, name: 'DC Thunder' },
+  ];
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
-  onItemClick() {
-    this.navCtrl.push(TeamDetailPage)
+  onItemClick(ev: any, team: Team) {
+    this.navCtrl.push(TeamDetailPage, { team: team });
   }
 
 }
