307b779ab95817b69735a4f90ad0903f5d90e84a
Alexandru Florian Barascu <alex.florin235@yahoo.com>
2017-10-01 19:22:17 +0300

    Added some hardcoded favourite teams and displayed them.

--- src/pages/my-teams/my-teams.ts
+++ src/pages/my-teams/my-teams.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, LoadingController, NavController } from 'ionic-angular';
 import { TournamentsPage } from "../tournaments/tournaments";
 import { Globals } from "../../app/shared/globals";
+import { EliteApiService } from "../../app/shared/elite-api.service";
+import {TeamHomePage} from "../team-home/team-home";
 
 @IonicPage({ name: Globals.PAGE_NAMES.myTeams })
 @Component({
@@ -9,11 +11,35 @@ import { Globals } from "../../app/shared/globals";
   templateUrl: 'my-teams.html',
 })
 export class MyTeamsPage {
+  public favourites: any[] = [
+    {
+      team: { id: 6182, name: 'HC Elite', coach: 'smt'},
+      tournamentId: '89e13aa2-ba6d-4f55-9cc2-61eba6172c63',
+      tournamentName: 'March Madness Tournament',
+    },
+  ];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  constructor(
+    protected navCtrl: NavController,
+    protected loadingCtrl: LoadingController,
+    protected eliteApi: EliteApiService,
+  ) { }
 
   goToTournaments(): void {
     this.navCtrl.push(TournamentsPage);
+  }
+
+  onFacClick(fav: any): void {
+    const loader = this.loadingCtrl.create({
+      content: 'Getting data...',
+      dismissOnPageChange: true,
+    });
+
+    loader.present().then(() => {
+      this.eliteApi.getTournament(fav.tournamentId).subscribe((t: any) => {
+        this.navCtrl.push(TeamHomePage, { team: fav.team });
+        loader.dismiss();
+      });
+    });
   }
 }
